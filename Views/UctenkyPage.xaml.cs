8f3d6df42cbe32b358837a61e509107a87f564b2
Juricze <33333093+Juricze@users.noreply.github.com>
2025-09-19 21:30:45 +0200

    db uctenek
    nahled a tisk uctenek

--- Views/UctenkyPage.xaml.cs
+++ Views/UctenkyPage.xaml.cs
@@ -1,6 +1,9 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
 using Sklad_2.ViewModels;
+using Sklad_2.Views.Dialogs;
+using System;
 
 namespace Sklad_2.Views
 {
@@ -17,6 +20,18 @@ namespace Sklad_2.Views
         {
             base.OnNavigatedTo(e);
             ViewModel.LoadReceiptsCommand.Execute(null); // Volání LoadReceiptsCommand zpět
+        }
+
+        private async void ShowPreviewButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ViewModel.SelectedReceipt != null)
+            {
+                var previewDialog = new ReceiptPreviewDialog(ViewModel.SelectedReceipt)
+                {
+                    XamlRoot = this.XamlRoot,
+                };
+                await previewDialog.ShowAsync();
+            }
         }
     }
 }
--- obj/x64/Debug/net8.0-windows10.0.19041.0/Sklad_2.AssemblyInfo.cs
+++ obj/x64/Debug/net8.0-windows10.0.19041.0/Sklad_2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sklad_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19519f36109068e49f020916ee44e6438935a655")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2b5309d8e1b9123e02d9dd542fa687643e5a75d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sklad_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sklad_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
